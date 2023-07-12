f0f33795835ebf9a6c9ca441e5358ed38176bd69
Cameron Dugan <cameron.dugan@protonmail.com>
2023-07-12 10:01:13 -0400

    upload after build

--- configuration.nix
+++ configuration.nix
@@ -29,6 +29,9 @@
       "org/gnome/desktop/peripherals/touchpad" = {
 	tap-to-click = true;
       };
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+	custom-keybindings = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"];
+      };
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
         binding = "<Super>t";
 	command = "kgx";
