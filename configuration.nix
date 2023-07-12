44cbbeebcba33c89e78634c9de4e48e52ac499a8
Cameron Dugan <cameron.dugan@protonmail.com>
2023-07-12 16:47:34 -0400

    upload after build

--- configuration.nix
+++ configuration.nix
@@ -242,7 +242,9 @@
     # List packages installed in system profile. To search, run:
     # $ nix search wget
     environment.systemPackages = with pkgs; [
-    # Game Software.
+        # Gnome
+        gnome.gnome-themes-extra
+        # Game Software.
         lutris
         bottles
         # Gnome Extensions.
