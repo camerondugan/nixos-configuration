4a80acf870d5f4e921db484560a817df96563530
Cameron Dugan <cameron.dugan@protonmail.com>
2023-07-14 08:27:41 -0400

    upload after build

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,7 @@
 
         programs.neovim = {
             enable = true;
+            defaultEditor = true;
             package = pkgs.neovim-unwrapped;
             viAlias = true;
             vimAlias = true;
