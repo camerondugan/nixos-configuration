5ffd24ebc0b7cddafc8d8e1361322fb26b5b04d9
Cameron Dugan <cameron.dugan@protonmail.com>
2023-07-14 09:57:26 -0400

    upload after build

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,6 @@
 { config, pkgs, ... }:
 
 {
-    nix.nixPath = [ "nixos-config=/home/cam/.nixos/configuration.nix" ];
     imports =
         [ # Include the results of the hardware scan.
             ./hardware-configuration.nix
