f9a85f93cf58a623307ce35839f435b653f288b8
Cameron Dugan <cameron.dugan@protonmail.com>
2023-07-16 14:50:55 -0400

    upload after build

--- configuration.nix
+++ configuration.nix
@@ -186,6 +186,7 @@
     # Yubikey Optional Unlock
     #security.pam.u2f.enable = true;
     #security.pam.u2f.cue = true;
+    services.udev.packages = [ pkgs.yubikey-personalization ];
     security.pam.services = {
         login.u2fAuth = true;
         sudo.u2fAuth = true;
