de21dd7de5ff019973726dfa8d9382c846d136b4
Cameron Dugan <cameron.dugan@protonmail.com>
2023-07-16 23:26:57 -0400

    fixed one-time-setup.sh

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -8,5 +8,12 @@ nix-shell -p git --run "git clone https://gitlab.com/cameron.dugan/nixos-configu
 # Set config as this one
 sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
 sudo ln -s /home/$USER/.nixos/configuration.nix /etc/nixos/configuration.nix
+# Setup Swap.nix
+echo "{ config, pkgs, ... }:
+
+{" >> ~/.nixos/swap.nix
+cat /etc/nixos/configuration.nix.bak | grep 'luks' >> ~/.nixos/swap.nix
+echo "
+}" >> ~/.nixos/swap.nix
 # Use config
 sudo nixos-rebuild switch
