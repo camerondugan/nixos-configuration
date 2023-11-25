7b5ece3d09eb3a899365272701a61998102acf24
Cameron Dugan <cameron.dugan@protonmail.com>
2023-11-24 23:45:37 -0500

    upload after build

--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-sudo nixos-rebuild switch -I nixos-config=/home/$USER/.nixos/configuration.nix && git add . && git commit -m 'upload after build' && git push
+sudo nixos-rebuild boot -I nixos-config=/home/$USER/.nixos/configuration.nix && git add . && git commit -m 'upload after build' && git push
