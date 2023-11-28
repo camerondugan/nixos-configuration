5e32f4a98194024aefbb3289adec4d785e0c6147
Cameron Dugan <cameron.dugan@protonmail.com>
2023-11-27 23:23:02 -0500

    upload after build

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -1,9 +1,8 @@
 # Set unstable and home-manager
-sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 
 # Set to unstable channels
-# sudo nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
-# sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 # sudo nix-channel --update
 
 # Download latest config
@@ -14,9 +13,10 @@ sudo ln -s /home/$USER/.nixos/configuration.nix /etc/nixos/configuration.nix
 # Setup Swap.nix
 echo "{ config, pkgs, ... }:
 
-{" >> ~/.nixos/swap.nix
-  cat /etc/nixos/configuration.nix.bak | grep 'luks' >> ~/.nixos/swap.nix
-  echo "
-}" >> ~/.nixos/swap.nix
+# Copy luks to swap.nix
+{" >>~/.nixos/swap.nix
+cat /etc/nixos/configuration.nix.bak | grep 'luks' >>~/.nixos/swap.nix
+echo "
+}" >>~/.nixos/swap.nix
 # Use config
 sudo nixos-rebuild switch
