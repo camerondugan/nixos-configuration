c9e8ede6f3b7fb7c87b3588d5aef87203b86d0d1
Cameron Dugan <cameron.dugan@protonmail.com>
2023-12-03 12:52:54 -0500

    upload after build

--- configuration.nix
+++ configuration.nix
@@ -396,30 +396,6 @@
             pfetch
             set fish_greeting
             fish_vi_key_bindings
-
-            function bind_bang
-                switch (commandline -t)[-1]
-                    case "!"
-                        commandline -t -- $history[1]
-                        commandline -f repaint
-                    case "*"
-                        commandline -i !
-                end
-            end
-            
-            function bind_dollar
-                switch (commandline -t)[-1]
-                    case "!"
-                        commandline -f backward-delete-char history-token-search-backward
-                    case "*"
-                        commandline -i '$'
-                end
-            end
-            
-            function fish_user_key_bindings
-                bind ! bind_bang
-                bind '$' bind_dollar
-            end
             '';
     };
     programs.hyprland.enable = true;
