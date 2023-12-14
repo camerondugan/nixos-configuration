fee2f1f337db89e4208ac3bddbc5d289bce48d25
Cameron Dugan <cameron.dugan@protonmail.com>
2023-12-13 19:53:10 -0500

    upload after build

--- upload.sh
+++ upload.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Switch
-if sudo nixos-rebuild switch --upgrade-all; then
+if sudo nixos-rebuild switch --upgrade; then
 	hyprctl reload
 	git add . && git commit -m 'upload after build' && git push
 else
 	# Build for next boot
-	if sudo nixos-rebuild boot --upgrade-all; then
+	if sudo nixos-rebuild boot --upgrade; then
 		hyprctl reload
 		git add . && git commit -m 'upload after build' && git push
 	fi
