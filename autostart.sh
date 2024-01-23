186102ede467896301490b3bd865f88cb15db0b3
Cameron Dugan <cameron.dugan@protonmail.com>
2024-01-23 13:22:34 -0500

    upload after build

--- autostart.sh
+++ autostart.sh
@@ -10,8 +10,12 @@ swaync &
 swayidle -w timeout 600 "hyprctl dispatcher dpms off" timeout 1200 "systemctl hibernate" resume "hyprctl dispatcher dpms on" &
 (yes | trash-empty 14) & # Empty trash more than 2 weeks old
 
+# Remove week old downloads
+find /home/cam/Downloads -mindepth 1 -mtime +7 -delete
+
 # Razer Hardware
 polychromatic-tray-applet &
+
 # Pull Git repos
 for repo in "/home/${USER}/.nixos" "/home/${USER}/.config/nvim" "/home/${USER}/.config/nvim/lua/user/"; do
 	( (cd "$repo" && git pull) || notify-send "\~/.nixos/autostart.sh: $repo failed to pull or does not exist")
