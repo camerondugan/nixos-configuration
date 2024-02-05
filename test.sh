95e7b44caed0f45d861529d72e52b6260db1ff84
Cameron Dugan <cameron.dugan@protonmail.com>
2024-02-05 14:18:30 -0500

    fixed scripts and added px to waybar.css

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+sudo nix-channel --update
 sudo nixos-rebuild switch --show-trace --fast
 hyprctl reload
 pkill waybar && (waybar >>/dev/null) &
--- upload.sh
+++ upload.sh
@@ -8,12 +8,12 @@ fi
 
 # Switch
 sudo nix-channel --update
-if sudo nixos-rebuild switch --upgrade --fast; then
+if sudo nixos-rebuild switch --upgrade --fast --show-trace; then
 	hyprctl reload
 	git add . && git commit -m "$arg1" && git push
 else
 	# Build for next boot
-	if sudo nixos-rebuild boot --upgrade --fast; then
+	if sudo nixos-rebuild boot --upgrade --fast --show-trace; then
 		hyprctl reload
 		git add . && git commit -m "$arg1" && git push
 	fi
