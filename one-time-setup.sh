2a79352b4351b3c9531ef544751ebb8989e81bb2
Cameron Dugan <cameron.dugan@protonmail.com>
2024-02-13 22:45:23 -0500

    added thinkpad trackpad and updated test.sh

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/usr/bin/env bash
 # Set unstable and home-manager
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager
 
@@ -11,16 +11,19 @@ sudo nix-channel --update
 nix-shell -p git --run "git clone https://gitlab.com/cameron.dugan/nixos-configuration.git"
 # Set config as this one
 sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
-sudo ln -s /home/$USER/.nixos/configuration.nix /etc/nixos/configuration.nix
+sudo ln -s /home/"$USER"/.nixos/configuration.nix /etc/nixos/configuration.nix
 # Setup Swap.nix
-echo "{ config, pkgs, ... }:
+{
+	echo "{ config, pkgs, ... }:
 
 # Copy luks to swap.nix
-{" >>~/.nixos/swap.nix
-cat /etc/nixos/configuration.nix.bak | grep 'luks' >>~/.nixos/swap.nix
-cat /etc/nixos/configuration.nix.bak | grep 'hostName' >>~/.nixos/swap.nix
-echo "
-}" >>~/.nixos/swap.nix
+{"
+	grep 'luks' /etc/nixos/configuration.nix.bak
+	grep 'hostName' /etc/nixos/configuration.nix.bak
+	echo "
+}"
+
+} >>~/.nixos/swap.nix
 # Use config
 sudo nixos-rebuild switch
 # Setup flathub (for extra options but probably won't need)
