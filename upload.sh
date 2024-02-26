27b6b79b19d1b049cf33cc6620313517fa275c22
Cameron Dugan <cameron.dugan@protonmail.com>
2024-02-26 14:52:07 -0500

    hotfix for gtk3+4 settings.ini home-manager [bc i didn]

--- upload.sh
+++ upload.sh
@@ -6,8 +6,18 @@ if [ -z "$arg1" ]; then
 	exit 1
 fi
 
+# Paths for GTK Settings.ini and .bak
+GTK3SETTINGSPATH="/home/$USER/.config/gtk-3.0/settings.ini"
+GTK4SETTINGSPATH="/home/$USER/.config/gtk-4.0/settings.ini"
+GTK3SETTINGSBAKPATH="/home/$USER/.config/gtk-3.0/settings.bak"
+GTK4SETTINGSBAKPATH="/home/$USER/.config/gtk-4.0/settings.bak"
+
 # Switch
 sudo nix-channel --update
+# Clear out gtk settings for home-manager
+mv "$GTK3SETTINGSPATH" "$GTK3SETTINGSBAKPATH"
+mv "$GTK4SETTINGSPATH" "$GTK4SETTINGSBAKPATH"
+
 if sudo nixos-rebuild switch --upgrade --fast --show-trace; then
 	# hyprctl reload
 	git add . && (
@@ -25,4 +35,12 @@ else
 			git push github
 		)
 	fi
+fi
+
+# Restore gtk3 settings if not generated for us
+if [ ! -f "$GTK3SETTINGSPATH" ]; then
+	cp "$GTK3SETTINGSBAKPATH" "$GTK3SETTINGSPATH"
+fi
+if [ ! -f "$GTK4SETTINGSPATH" ]; then
+	cp "$GTK4SETTINGSBAKPATH" "$GTK4SETTINGSPATH"
 fi
