7d565b5869a188a75c35f6c04290bb214f546733
Cameron Dugan <cameron.dugan@protonmail.com>
2024-02-29 10:47:02 -0500

    added more development pkgs

--- configuration.nix
+++ configuration.nix
@@ -452,6 +452,11 @@ in {
             jpegoptim
             ntfy-sh
             gaphor
+            glib
+            gobject-introspection
+            dbus-glib
+            gtk4
+            gtk3
 
             # Languages
             dotnet-sdk
@@ -484,7 +489,7 @@ in {
         VISUAL = "neovide";
         DOTNET_ROOT = "${pkgs.dotnet-sdk}";
         NODE_PATH = "~/.system_node_modules/lib/node_modules";
-        WEBKIT_FORCE_SANDBOX = "0"; # gnome accounts fix
+        PKG_CONFIG_PATH = "/run/current-system/sw/lib/pkgconfig:$PKG_CONFIG_PATH";
     };
 
     environment.gnome.excludePackages = [ 
