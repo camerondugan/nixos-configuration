46175bcb72c84aa2e907659d867a7de31ef8014d
Cameron Dugan <cameron.dugan@protonmail.com>
2024-03-21 13:07:20 -0400

    Ignore this-device.nix

--- this-device.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, pkgs, modulesPath, ...}:
-
-{
-  networking.hostName = "Desktop"; # Define your hostname.
-  # Enable swap on luks
-  boot.initrd.luks.devices."luks-0901906b-019c-4d9c-aaf1-5a0a4eaf7ea3".device = "/dev/disk/by-uuid/0901906b-019c-4d9c-aaf1-5a0a4eaf7ea3";
-  boot.initrd.luks.devices."luks-0901906b-019c-4d9c-aaf1-5a0a4eaf7ea3".keyFile = "/crypto_keyfile.bin";
-}
