1454fb42bca7086f2c326c8b9203cb03caeb0735
Cameron Dugan <cameron.dugan@protonmail.com>
2025-07-05 19:49:58 -0400

    fix: desktop suspend resume works now

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   imports = [
     ./hardware-configuration.nix
     ../../home-modules
@@ -8,10 +8,6 @@
 
   networking.hostName = "Desktop";
 
-  services = {
-    ollama.package = pkgs.ollama-cuda;
-  };
-
   # swapDevices = [
   #   {
   #     device = "/swapfile";
@@ -19,5 +15,28 @@
   #   }
   # ];
 
+  # Suspend fix
+  boot.kernelParams = [
+    "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+    "nvidia.NVreg_TemporaryFilePath=/tmp"
+  ];
+  hardware.nvidia.powerManagement.enable = true;
+
   hardware.nvidia.open = true;
+  # hardware.nvidia.powerManagement.finegrained = true;
+
+  # monitor audio prevent sleep
+  services.pipewire.wireplumber.extraConfig."99-disable-suspend" = {
+    "monitor.alsa.rules" = [
+      {
+        matches = [
+          { "node.name" = "alsa_output.pci-0000_0a_00.1.hdmi-stereo";}
+        ];
+        actions.update-props = {
+          "session.suspend-timeout-seconds" = 0;
+          "node.always-process" = true;
+        };
+      }
+    ];
+  };
 }
