82faffe34d7d424fbd7493282d3ec3205325c502
Cameron Dugan <cameron.dugan@protonmail.com>
2025-08-06 18:15:01 -0400

    fix: make inactive windows identifiable

--- nix-modules/coding/default.nix
+++ nix-modules/coding/default.nix
@@ -34,18 +34,18 @@
             enable_transience
             set NIXPKGS_ALLOW_UNFREE 1
             set EDITOR $(which hx)
-            set ZELLIJ_AUTO_EXIT "true"
-            if not set -q ZELLIJ
-                if test "$ZELLIJ_AUTO_ATTACH" = "true"
-                    zellij attach -c
-                else
-                    zellij
-                end
+            # set ZELLIJ_AUTO_EXIT "true"
+            # if not set -q ZELLIJ
+                # if test "$ZELLIJ_AUTO_ATTACH" = "true"
+                    # zellij attach -c
+                # else
+                    # zellij
+                # end
 
-                if test "$ZELLIJ_AUTO_EXIT" = "true"
-                    kill $fish_pid
-                end
-            end
+                # if test "$ZELLIJ_AUTO_EXIT" = "true"
+                    # kill $fish_pid
+                # end
+            # end
           '';
         shellAliases = {
           # doesn't show these changes to user
