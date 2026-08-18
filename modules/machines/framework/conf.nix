d634eee51459b14dff21de673cc7b61c8f00cb65
Cameron Dugan <me@camerondugan.com>
2026-08-18 19:19:08 -0400

    fix(framework): remove temp setting

--- modules/machines/framework/conf.nix
+++ modules/machines/framework/conf.nix
@@ -1,6 +1,6 @@
 {
   flake.nixosModules.frameworkConf = {
-    services.pipewire.alsa.support32Bit = false; # Temporary
+    # services.pipewire.alsa.support32Bit = false; # Temporary
     networking.hostName = "Framework13";
     services.fwupd.enable = true;
   };
